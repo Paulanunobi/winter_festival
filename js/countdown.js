14ed034d1df294504d2c73b07a40c4a41acb2901
lunatiqueue <vadym.kochenko@gmail.com>
2023-12-12 10:51:45 -0500

    minor changes
    minor changes

---
+++ js/countdown.js
@@ -0,0 +1,31 @@
+let daysToNewyear = document.getElementById("days-to-newyear");
+let hoursToNewyear = document.getElementById("hours-to-newyear");
+let minutesToNewyear = document.getElementById("minutes-to-newyear");
+let secondsToNewyear = document.getElementById("seconds-to-newyear");
+let newyearTitle = document.getElementById("newyear-title");
+let newYearDate = new Date(2024, 1, 1);
+let neyYearTime = newYearDate.getTime();
+
+let oneMin = 60 * 1000;
+let oneHour = 60 * oneMin;
+let oneDay = 24 * oneHour;
+
+function countdown() {
+  let todayDate = new Date();
+  let todayTime = todayDate.getTime();
+
+  if (neyYearTime >= todayTime) {
+    let remainingTime = neyYearTime - todayTime;
+    daysToNewyear.textContent = Math.floor(remainingTime / oneDay);
+    hoursToNewyear.textContent = Math.floor((remainingTime % oneDay) / oneHour);
+    minutesToNewyear.textContent = Math.floor(
+      (remainingTime % oneHour) / oneMin
+    );
+    secondsToNewyear.textContent = Math.floor((remainingTime % oneMin) / 1000);
+  } else {
+    newyearTitle.setAttribute("style", "color: red");
+    newyearTitle.textContent = "Countdown has expired";
+  }
+}
+setInterval(countdown, 1000);
+countdown();
